fb519f0f5e84b389c8040d95d5a28dc8ec02d3e4
Jongseob Yun <cjsgkwkd4@gmail.com>
2025-03-26 21:28:07 +0900

    folderName을 내부에서 만들어내도록 수정정

--- getJobsInFolder.groovy
+++ getJobsInFolder.groovy
@@ -1,18 +1,21 @@
 import jenkins.model.*
 import com.cloudbees.hudson.plugins.folder.Folder // required to check if the item is a folder (instanceof Folder)
 
-def getJobsInFolder(String folderPath, boolean recursive = false) {
-    def folder = Jenkins.get().getItemByFullName(folderPath)
-    if (!folder) return []
+// Example) 
+// return [job0, job1, job2, sub_folder/job0, sub_folder/job1]
+def getJobsInFolder(boolean recursive = false) {
+    def currFolderName = env.JOB_NAME.split('/')[0] // type: string
+    def currFolder = Jenkins.get().getItemByFullName(currFolderName) // type: com.cloudbees.hudson.plugins.folder.Folder
+    if (!currFolder) return []
 
     def jobs = []
-    def queue = [folder]
+    def queue = [currFolder]
 
     while (queue) {
         def currentFolder = queue.remove(0)
         currentFolder.getItems().each { item ->
             if (item instanceof Job) {
-                jobs.add(item.fullName.replaceFirst("^${folderPath}/", ''))
+                jobs.add(item.fullName.replaceFirst("^${currFolderName}/", ''))
             } else if (recursive && item instanceof Folder) {
                 queue << item // Add the sub-folder to the queue to process its items
             }
@@ -24,19 +27,14 @@ def getJobsInFolder(String folderPath, boolean recursive = false) {
     jobs.findAll { it != currentJobName }
 }
 
-
-// Test code: echo all jobs from the function
 node {
     echo "Current job: ${env.JOB_NAME}"
-    def folderName = env.JOB_NAME.split('/')[0]
-    def jobs = getJobsInFolder(folderName, true)
+    def jobs = getJobsInFolder()
     echo "Jobs including sub-folders: ${jobs}"
 }
 
-// Test code: Run all jobs from the function in each stage
 node {
-    def folderName = env.JOB_NAME.split('/')[0]
-    def jobs = getJobsInFolder(folderName, true)
+    def jobs = getJobsInFolder()
 
     jobs.each { job ->
         stage("Run ${job}") {
